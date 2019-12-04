c3d0b6ddae5bc48c7144b889d626734962ef3784
Victor Johnson <johnsonsirv@gmail.com>
2019-12-04 16:53:44 +0100

    added spec for game

---
+++ spec/GameSpec.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/extensions */
+
+import Player from '../src/js/player.js';
+import GameUI from '../src/js/gameui.js';
+import Game from '../src/js/game.js';
+
+describe('Name of the group', () => {
+  
+});
